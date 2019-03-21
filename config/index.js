699ca144307a6df01b0e8d397e66e74414c4c9e5
zhoucumt <553817478@qq.com>
2019-03-21 14:15:00 +0800

    update token

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@
 exports.baseURL = 'https://api.github.com';
 // github 组织名称
 exports.orgName = 'ivue-app-cli';
-exports.token = 'f794fbf7acde513ddf5374b5ba0d9777a5d6878a';
+exports.token = '5 e 1 b 9 2 7 b b d d b 0 b 1 7 b 0 4 7 d f 9 a 4 8 a 3 c 7 2 a 7 3 c 6 0 3 7 e';
